3cd39f25532876cc2ea63359bb93785c405b7235
Marcin Skic <marcinskicoficjal@gmail.com>
2025-01-25 21:50:41 +0100

    feat: add option to close and open UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    private bool menuOpen = false;
+    [SerializeField]
+    private GameObject menu;
+    [SerializeField]
+    private GameObject howToOpenMenu;
+    [SerializeField]
+    private GameObject howToCloseMenu;
     [SerializeField]
     private Slider healthBar;
     [SerializeField]
@@ -20,6 +30,7 @@ public class UIManager : MonoBehaviour
     {
         healthBar.maxValue = GameManager.Instance.MaxBabyHealth;
         gameLostText.gameObject.SetActive(false);
+        UpdateMenuState();
         StartCoroutine(UIUpdate());
     }
 
@@ -27,14 +38,12 @@ public class UIManager : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(0.5f);
-            
             //health
             healthBar.value = GameManager.Instance.BabyHealth;
 
             //upgrades
             var enemyKillStatistics = GameManager.Instance.enemyKillStatistics;
-            dirtCleanedText.text = $"{(enemyKillStatistics.TryGetValue("Dirt",out var dirtCount) ? dirtCount : 0)} Dirt";
+            dirtCleanedText.text = $"{(enemyKillStatistics.TryGetValue("Dirt", out var dirtCount) ? dirtCount : 0)} Dirt";
             virusCleanedText.text = $"{(enemyKillStatistics.TryGetValue("Virus", out var virusCount) ? virusCount : 0)} Virus";
             bacteriaCleanedText.text = $"{(enemyKillStatistics.TryGetValue("Bacteria", out var bacteriaCount) ? bacteriaCount : 0)} Bacteria";
 
@@ -44,6 +53,41 @@ public class UIManager : MonoBehaviour
                 gameLostText.gameObject.SetActive(true);
                 gameLostText.text = GameManager.Instance.DeathReasonText;
             }
+
+            yield return new WaitForSeconds(0.1f);
         }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Gamepad.current?.leftStick.ReadValue()[1] < 0)
+        {
+            CloseMenu();
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Gamepad.current?.leftStick.ReadValue()[1] > 0)
+        {
+            OpenMenu();
+        }
+    }
+
+    private void OpenMenu()
+    {
+        menuOpen = true;
+        UpdateMenuState();
+
+    }
+
+    private void CloseMenu()
+    {
+        menuOpen = false;
+        UpdateMenuState();
+    }
+
+    private void UpdateMenuState()
+    {
+        menu.SetActive(menuOpen);
+        howToOpenMenu.SetActive(!menuOpen);
+        howToCloseMenu.SetActive(menuOpen);
     }
 }
