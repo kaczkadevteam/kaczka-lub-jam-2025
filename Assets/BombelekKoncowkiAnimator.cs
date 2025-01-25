060d79b3704ed46cd3b9b2ae8b75123d1b206cfc
Filip Tarajko <76916685+FilipTarajko@users.noreply.github.com>
2025-01-25 19:57:39 +0100

    feat: ADD BOMBELEK

---
+++ Assets/BombelekKoncowkiAnimator.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombelekKoncowkiAnimator : MonoBehaviour
+{
+    // [SerializeField] private List<GameObject> koncowkiBombelka;
+    // [SerializeField] private bool areKoncowkisGoingUp;
+    // [SerializeField] private float koncowkisZ;
+
+    // void Update()
+    // {
+    //     koncowkisZ += 0.1f;
+    //     foreach (var koncowka in koncowkiBombelka)
+    //     {
+    //         koncowka.transform.rotation = new Vector3(koncowka.transform.rotation.x, koncowka.transform.rotation.y, koncowkisZ);
+    //     }
+    // }
+}
