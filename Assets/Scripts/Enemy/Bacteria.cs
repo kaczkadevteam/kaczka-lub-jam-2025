8bbdce869e2e8f3d9f383d8484b89bc89bbe327e
Maciej Potręć <potrec123@proton.me>
2025-01-25 15:54:17 +0100

    feat: add replicate to bacteria

--- Assets/Scripts/Enemy/Bacteria.cs
+++ Assets/Scripts/Enemy/Bacteria.cs
@@ -1,9 +1,31 @@
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Bacteria : EnemyBase
 {
+    private GameObject rootObject;
     // todo 
-    private void Replicate()
+    private IEnumerator Replicate()
     {
+        while (true)
+        {
+            yield return new WaitForSeconds(10f);
+            Vector3 randomSpawnPointNearby = GetRandomSpawnPointNearby();
+            EnemySpawnerManager.Instance.SpawnEnemy(rootObject.transform.position + randomSpawnPointNearby, enemySO);
+        }
+    }
+    
+    private Vector3 GetRandomSpawnPointNearby()
+    {
+        Vector3 randomSpawnPointNearby = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
+        return randomSpawnPointNearby;
+    }
+
+    void Start()
+    {
+        base.Start();
+        rootObject = transform.gameObject;
+        StartCoroutine(Replicate());
     }
 }
